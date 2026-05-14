5b707ef0008d7426263146b9a4aad112468903f5
alessio826 <al3on.multiplatform2026@gmail.com>
2026-05-14 19:27:54 +0200

    Refactor PostCSS config and add class name utility
    Removed PostCSS plugins configuration and added a utility function for class name merging.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-export default {
-  plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
